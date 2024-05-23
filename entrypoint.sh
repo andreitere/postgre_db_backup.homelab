c231b3a9a33c02c2a273aa2b3bacc71c32e12b1c
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2024-05-23 22:13:11 +0300

    proper build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#
+
 # Log the CRON_SCHEDULE
 echo "Setting up cron job with schedule: $CRON_SCHEDULE"
 echo "Setting up cron job with schedule: $CRON_SCHEDULE" >> /var/log/cron.log
 
 # Create a cron job file
-echo "$CRON_SCHEDULE root /usr/local/bin/backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
+echo "$CRON_SCHEDULE /usr/local/bin/backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
 
-# Give execution rights on the cron job
+# Give execution rights on the cron job file
 chmod 0644 /etc/cron.d/backup-cron
 
-# Apply cron job
+# Apply the cron job
 crontab /etc/cron.d/backup-cron
 
-# Start cron and tail log file
+# Start cron and tail the log file
 cron && tail -f /var/log/cron.log
-
